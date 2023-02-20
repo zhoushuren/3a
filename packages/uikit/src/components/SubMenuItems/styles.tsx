e480563c75ae87fa72cc8cd02e43036eccd7dd4c
huda <huda@gmail.com>
2023-02-20 23:21:51 +0800

    修复一版样式

--- packages/uikit/src/components/SubMenuItems/styles.tsx
+++ packages/uikit/src/components/SubMenuItems/styles.tsx
@@ -13,7 +13,7 @@ const StyledSubMenuItems = styled(Flex)`
   position: relative;
   z-index: 1;
   width: 100%;
-  display: block;
+  display: flex;
   white-space: nowrap;
   scroll-behavior: smooth;
   ${({ theme }) => theme.mediaQueries.md} {
@@ -29,6 +29,7 @@ const StyledSubMenuItems = styled(Flex)`
   &::-webkit-scrollbar {
     display: none;
   }
+  padding-top:14px;
 `;
 
 const maskSharedStyle = css`
